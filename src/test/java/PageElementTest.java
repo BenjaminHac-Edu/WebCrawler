5d0fb738266190c56e720f5ab424e91a75079802
Benjamin Hacksteiner <benjaminhac@edu.aau.at>
2025-04-16 10:36:34 +0200

    test: add testcases for PageElement subclasses

---
+++ src/test/java/PageElementTest.java
@@ -0,0 +1,47 @@
+import com.webcrawler.crawler.BrokenLink;
+import com.webcrawler.crawler.Heading;
+import com.webcrawler.crawler.Link;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PageElementTest {
+    @Test
+    void testHeadingDepth() {
+        Heading heading = new Heading(2, "Section Title");
+        assertEquals(2, heading.getDepth());
+    }
+
+    @Test
+    void testHeadingToMarkdown() {
+        Heading heading = new Heading(2, "Section Title");
+        String result = heading.toMarkdown("-->");
+        assertEquals("# --> Section Title", result);
+    }
+
+    @Test
+    void testLinkDepth() {
+        Link link = new Link(1, "https://example.com");
+        assertEquals(1, link.getDepth());
+    }
+
+    @Test
+    void testLinkToMarkdown() {
+        Link link = new Link(1, "https://example.com");
+        String result = link.toMarkdown("");
+        assertEquals("<br> link to <a>https://example.com</a>", result);
+    }
+
+    @Test
+    void testBrokenLinkDepth() {
+        BrokenLink broken = new BrokenLink(3, "https://404.com");
+        assertEquals(3, broken.getDepth());
+    }
+
+    @Test
+    void testBrokenLinkToMarkdown() {
+        BrokenLink broken = new BrokenLink(3, "https://404.com");
+        String result = broken.toMarkdown("---->");
+        assertEquals("<br>----> broken link <a>https://404.com</a>", result);
+    }
+}
